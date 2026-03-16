d71e86cf65633320db023ed342e01853ae1a7c69
kavya <kavyaanil299@gmail.com>
2026-03-16 20:52:29 +0530

    updated api url

--- ecommerce-frontend/src/api/axiosInstance.js
+++ ecommerce-frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
- baseURL:"http://localhost:5000/api"
+ baseURL:"https://ecommerce-project-4-rt1q.onrender.com"
 })
 
 API.interceptors.request.use((req)=>{
